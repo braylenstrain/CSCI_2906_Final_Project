8cdf9bd8b98f6a1961887fa03369a19ff2541e16
404kevnotfound <76047181+braylenstrain@users.noreply.github.com>
2022-04-25 15:07:13 -0600

    Create button to move on to each minigame

--- Capstone.java
+++ Capstone.java
@@ -1,4 +1,3 @@
-package application;
 
 //TODO display the minigames (if it's level 1, display instructions first), have a countdown
 //TODO how to deal with losses in both minigames and overall. Need a restart option.
@@ -11,16 +10,18 @@ package application;
  */
 import javafx.application.*;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.Scene;
 import javafx.scene.text.*;
-import javafx.animation.*;
-import javafx.event.*;
+import javafx.scene.control.Button;
 
 public class Capstone extends Application{
 	private int level = 1;
 	private static int lives = 3;
+	private static int preMinigameLives = 3;
+	public static Button btNextMinigame = new Button("Next");
+	private Pane pane;
+	private final int WINDOW_SIZE = 700;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -36,66 +37,72 @@ public class Capstone extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		StackPane pane = new StackPane(); // The main pane that will hold all the minigame panes
-		Text introductionText = new Text("Welcome to my game."); //Intro text explaining the game
+		btNextMinigame.setPrefSize(200, 100); //Make button bigger so it's more noticeable
+		btNextMinigame.setFont(Font.font(40));
+		pane = new StackPane(); //The pane that will show the introduction
+		Text introductionText = new Text("Welcome to my game."); //Introduction text explaining the game
 		
 		//Increase size of intro text and put into pane
 		introductionText.setFont(Font.font(30));
 		pane.getChildren().add(introductionText);
 		
 		//Set the scene and stage
-		Scene scene = new Scene(pane, 700, 700);
+		Scene scene = new Scene(pane, WINDOW_SIZE, WINDOW_SIZE);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Can You Beat My Game?");
 		primaryStage.show();
 		primaryStage.setResizable(false);
-		
-		//Handler for changing introductionText
-//		EventHandler<ActionEvent> introEventHandler = e -> {
-//			switch(introCount) {
-//			case 0: introductionText.setText("Do you think you can beat it?");
-//					introCount++;
-//					break;
-//			case 1: introductionText.setText("We will see.");
-//					introCount++;
-//					break;
-//			case 2: introductionText.setText("You have 3 lives to beat 3 levels.\nThere are 5 minigames per level.\nLose a minigame, lose a life.\nLose all your lives, game over.\n\nPress any key on the keyboard to continue...");
-//					introCount++;
-//			}
-//		};
-//		
-//		//Animation that changes introductionText contents
-//		Timeline introAnimation = new Timeline(new KeyFrame(Duration.millis(3000), introEventHandler));
-//		introAnimation.setCycleCount(3);
-//		introAnimation.play();
 
 		//Create a thread to introduce the game, changing the contents of introductionText periodically, then calling runTheGame()
 		new Thread(() -> {
 			try {
-				Thread.sleep(3000);
+				Thread.sleep(000);
 				introductionText.setText("Do you think you can beat it?");
-				Thread.sleep(3000);
+				Thread.sleep(000);
 				introductionText.setText("We will see.");
-				Thread.sleep(3000);
+				Thread.sleep(000);
 				introductionText.setText("You have 3 lives to beat 3 levels.\nThere are 5 minigames per level.\nLose a minigame, lose a life.\nLose all your lives, game over.");
-				Thread.sleep(6000);
+				Thread.sleep(000);
 				Platform.runLater(() -> pane.getChildren().clear());
-				Platform.runLater(() -> runTheGame(primaryStage));
+				Platform.runLater(() -> startTheGame(primaryStage));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}).start();
 		
+		btNextMinigame.setOnAction(e -> {
+			if (pane instanceof CirclesMinigame) {
+				runSecondMinigame(primaryStage);
+			}
+		});
 	}
 	
+	
+	
 	//Run the rest of the game after the intro
-	private void runTheGame(Stage primaryStage) {
+	private void startTheGame(Stage primaryStage) {
 		//Run the first minigame
-		CirclesMinigame mini1 = new CirclesMinigame(level);
-		Scene scene = new Scene(mini1, 700, 700);
+		pane = new CirclesMinigame(level);
+		Scene scene = new Scene(pane, WINDOW_SIZE, WINDOW_SIZE);
 		primaryStage.setScene(scene);
-		//TODO how to wait for each minigame to finish?
 		//TODO Measure if last minigame was won or lost based on lives remaining vs started, have an inbetween pane with text
+	}
+	
+	private void runSecondMinigame(Stage primaryStage) {
+		if (preMinigameLives != lives) {
+			lostALife(primaryStage);
+			preMinigameLives = lives;
+		} else {
+			System.out.println("Won");
+		}
+	}
+	
+	private void lostALife(Stage primaryStage) {
+		Text loseText = new Text("You lost a life.\nYou have " + lives + " remaining.");
+		loseText.setFont(Font.font(30));
+		pane = new StackPane(loseText);
+		Scene scene = new Scene(pane, WINDOW_SIZE, WINDOW_SIZE);
+		primaryStage.setScene(scene);
 	}
 
 }
