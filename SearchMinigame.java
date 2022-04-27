b17807f505523b27c2bd8421c6636b9648ea1aff
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-04-27 14:00:52 -0600

    Update SearchMinigame.java

--- SearchMinigame.java
+++ SearchMinigame.java
@@ -3,12 +3,16 @@ import java.util.ArrayList;
 
 import javafx.scene.layout.*;
 import javafx.scene.Node;
+import javafx.scene.image.*;
 
 public class SearchMinigame extends BorderPane {
 	//TODO node mouse enter/leave, arraylist of nodes shuffled, randomly choose node to find, difficulty increases by number of nodes and time
-	ArrayList<Node> nodeList = new ArrayList<>();
+	private ArrayList<Node> nodeList = new ArrayList<>();
 	
 	public SearchMinigame(int level) {
+		for (int i = 0; i < 15; i++ ) {
+			
+		}
 		if (level == 1) {
 			level1();
 		} else if (level == 2) {
