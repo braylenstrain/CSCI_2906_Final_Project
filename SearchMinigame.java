441bc236c31c4c26c25e66084b28358f3f500230
404kevnotfound <76047181+braylenstrain@users.noreply.github.com>
2022-04-27 16:19:37 -0600

    Update image source file names

--- SearchMinigame.java
+++ SearchMinigame.java
@@ -2,33 +2,34 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
 import javafx.scene.layout.*;
 import javafx.scene.image.*;
-import javafx.scene.control.ScrollPane;
 
+//TODO node mouse enter/leave, randomly choose node to find, difficulty increases by number of nodes and time
 public class SearchMinigame extends BorderPane {
-	//TODO node mouse enter/leave, arraylist of nodes shuffled, randomly choose node to find, difficulty increases by number of nodes and time
+	private static final int PANE_SIZE = 700;
 	private List<ImageView> imageList = Arrays.asList(
-			new ImageView(new Image("images/2022_04_27_0m8_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0mb_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0md_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0mi_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0mk_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0ml_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0mn_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0mo_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0mp_Kleki.png")),
-			new ImageView(new Image("images/2022_04_27_0ms_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0m8_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0mb_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0md_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0mi_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0mk_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0ml_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0mn_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0mo_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0mp_Kleki-removebg-preview.png")),
+			new ImageView(new Image("images/2022_04_27_0ms_Kleki-removebg-preview.png")),
 			new ImageView(new Image("images/23710-8-legolas-transparent-image.png")),
-			new ImageView(new Image("images/388-3880363_captain-jack-sparrow-png.png")),
-			new ImageView(new Image("images/8e6b064c27cdda51e81589dc11ac68bf.jpeg")),
+			new ImageView(new Image("images/388-3880363_captain-jack-sparrow-png-removebg-preview.png")),
+			new ImageView(new Image("images/8e6b064c27cdda51e81589dc11ac68bf-removebg-preview.png")),
 			new ImageView(new Image("images/Avatar_Aang.png")),
 			new ImageView(new Image("images/Master-Chief-Transparent-Background.png"))
 			);
+	private Pane pane = new Pane();
 	
 	public SearchMinigame(int level) {
-		Collections.shuffle(imageList);
+		Collections.shuffle(imageList); //Shuffle imageList so each SearchMinigame has different images/image locations
+		pane.setStyle("-fx-backgorund-color: white");
 		if (level == 1) {
 			level1();
 		} else if (level == 2) {
@@ -38,15 +39,17 @@ public class SearchMinigame extends BorderPane {
 		}
 	}
 
+	//Put 8 pictures behind pane to search through
 	private void level1() {
-		for (ImageView image: imageList) {
+		for (int i = 0; i < 8; i++) {
+			ImageView image = imageList.get(i);
 			image.setPreserveRatio(true);
 			image.setFitHeight(100);
+			image.setX((int)(Math.random() * (PANE_SIZE - 100)));
+			image.setY((int)(Math.random() * (PANE_SIZE - 200)));
+			pane.getChildren().add(image);
 		}
-		FlowPane test = new FlowPane();
-		test.getChildren().addAll(imageList);
-		setCenter(test);
-		
+		setCenter(pane);
 	}
 	
 	private void level2() {
