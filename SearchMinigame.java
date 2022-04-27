44f9ec3e71eefcadc2f2d1eb61597e29cfa13db4
404kevnotfound <76047181+braylenstrain@users.noreply.github.com>
2022-04-27 15:42:33 -0600

    Create imageList

--- SearchMinigame.java
+++ SearchMinigame.java
@@ -1,18 +1,34 @@
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javafx.scene.layout.*;
-import javafx.scene.Node;
 import javafx.scene.image.*;
+import javafx.scene.control.ScrollPane;
 
 public class SearchMinigame extends BorderPane {
 	//TODO node mouse enter/leave, arraylist of nodes shuffled, randomly choose node to find, difficulty increases by number of nodes and time
-	private ArrayList<Node> nodeList = new ArrayList<>();
+	private List<ImageView> imageList = Arrays.asList(
+			new ImageView(new Image("images/2022_04_27_0m8_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0mb_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0md_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0mi_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0mk_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0ml_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0mn_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0mo_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0mp_Kleki.png")),
+			new ImageView(new Image("images/2022_04_27_0ms_Kleki.png")),
+			new ImageView(new Image("images/23710-8-legolas-transparent-image.png")),
+			new ImageView(new Image("images/388-3880363_captain-jack-sparrow-png.png")),
+			new ImageView(new Image("images/8e6b064c27cdda51e81589dc11ac68bf.jpeg")),
+			new ImageView(new Image("images/Avatar_Aang.png")),
+			new ImageView(new Image("images/Master-Chief-Transparent-Background.png"))
+			);
 	
 	public SearchMinigame(int level) {
-		for (int i = 0; i < 15; i++ ) {
-			
-		}
+		Collections.shuffle(imageList);
 		if (level == 1) {
 			level1();
 		} else if (level == 2) {
@@ -23,7 +39,13 @@ public class SearchMinigame extends BorderPane {
 	}
 
 	private void level1() {
-		// TODO Auto-generated method stub
+		for (ImageView image: imageList) {
+			image.setPreserveRatio(true);
+			image.setFitHeight(100);
+		}
+		FlowPane test = new FlowPane();
+		test.getChildren().addAll(imageList);
+		setCenter(test);
 		
 	}
 	
