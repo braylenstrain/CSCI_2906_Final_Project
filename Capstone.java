91daf82b68acb590103e3ed87147d1795e4f2dff
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-05-01 05:02:30 -0600

    Change inbetween messages to positive

--- Capstone.java
+++ Capstone.java
@@ -1,3 +1,4 @@
+package application;
 //TODO display the minigames (if it's level 1, display instructions first)
 //TODO how to deal with losses in both minigames and overall. Need a restart option.
 //TODO final end game win "I never doubted you for a second"
@@ -70,13 +71,13 @@ public class Capstone extends Application{
 		new Thread(() -> {
 			try {
 				//TODO fix sleep numbers 3000, 6000
-				Thread.sleep(3000);
+				Thread.sleep(000);
 				introductionText.setText("Do you think you can beat it?");
-				Thread.sleep(3000);
+				Thread.sleep(000);
 				introductionText.setText("We will see.");
-				Thread.sleep(3000);
+				Thread.sleep(000);
 				introductionText.setText("You have 3 lives to beat 3 levels.\nThere are 5 minigames per level.\nLose a minigame, lose a life.\nLose all your lives, game over.");
-				Thread.sleep(6000);
+				Thread.sleep(000);
 				Platform.runLater(() -> pane.getChildren().clear());
 				Platform.runLater(() -> runFirstMinigame(primaryStage));
 			} catch (InterruptedException e) {
@@ -198,25 +199,25 @@ public class Capstone extends Application{
 		Text winText = new Text();
 		winText.setFont(Font.font(25));
 		switch(randomText) {
-		case 0: winText.setText("Big whoop, you won one."); break;
-		case 1: winText.setText("Don't get cocky now."); break;
-		case 2: winText.setText("That one was easy."); break;
-		case 3: winText.setText("You actually think you can win?"); break;
-		case 4: winText.setText("Ugh, good job I guess."); break;
-		case 5: winText.setText("What, you want a medal or something?"); break;
-		case 6: winText.setText("You'll choke, I just know it."); break;
-		case 7: winText.setText("You're not as good as you think you are."); break;
-		case 8: winText.setText("I could have done that with me eyes closed."); break;
-		case 9: winText.setText("My grandma says you're trash."); break;
-		case 10: winText.setText("Beep Beep Boop, major loser detected."); break;
-		case 11: winText.setText("Why do you even try?"); break;
-		case 12: winText.setText("Your shoes are ugly."); break;
-		case 13: winText.setText("I don't have faith in you."); break;
-		case 14: winText.setText("Anyone could have beaten that one."); break;
-		case 15: winText.setText("I sense your confidence waining..."); break;
-		case 16: winText.setText("Give up."); break;
-		case 17: winText.setText("Whatever, it doesn't matter because you'll lose the next one."); break;
-		case 18: winText.setText(">:("); break;
+		case 0: winText.setText("Alright! You won one!"); break;
+		case 1: winText.setText("Stay focused."); break;
+		case 2: winText.setText("You made that look easy!"); break;
+		case 3: winText.setText("You can win."); break;
+		case 4: winText.setText("Good Job!"); break;
+		case 5: winText.setText("You deserve a cookie for that one!"); break;
+		case 6: winText.setText("Trust your feelings."); break;
+		case 7: winText.setText("You're pretty good!"); break;
+		case 8: winText.setText("I am so impressed!"); break;
+		case 9: winText.setText("My grandma says you're really good."); break;
+		case 10: winText.setText("Beep Beep Boop, major cool person detected."); break;
+		case 11: winText.setText("Don't stop believing!"); break;
+		case 12: winText.setText("Your shoes are really cool!"); break;
+		case 13: winText.setText("I have faith in you!"); break;
+		case 14: winText.setText("You're better at this then most."); break;
+		case 15: winText.setText("I sense your confidence increasing..."); break;
+		case 16: winText.setText("Don't give up."); break;
+		case 17: winText.setText("Nice! I bet you'll win the next one too!"); break;
+		case 18: winText.setText("=D"); break;
 		case 19: winText.setText("Nice work!"); break;
 		default: winText.setText("ERROR");
 		}
