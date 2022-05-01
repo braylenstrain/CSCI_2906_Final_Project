7e3b20bf0b97fb8be8497baedd3e4fc15c68ac3a
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-05-01 05:03:27 -0600

    Create DodgeMinigame

---
+++ DodgeMinigame.java
@@ -0,0 +1,26 @@
+package application;
+
+public class DodgeMinigame {
+
+	public DodgeMinigame(int level) {
+		if (level == 1) {
+			level1();
+		} else if (level == 2) {
+			level2();
+		} else {
+			level3();
+		}
+	}
+	
+	private void level1() {
+		
+	}
+	
+	private void level2() {
+		
+	}
+	
+	private void level3() {
+		
+	}
+}
