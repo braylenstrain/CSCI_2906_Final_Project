5a9eb0134b97150d7d54c6c7536915c21ed4b7af
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-05-01 23:45:58 -0600

    Make full game work again

--- Capstone.java
+++ Capstone.java
@@ -73,13 +73,13 @@ public class Capstone extends Application{
 		new Thread(() -> {
 			try {
 				//TODO fix sleep numbers 3000, 6000
-				Thread.sleep(000);
+				Thread.sleep(3000);
 				introductionText.setText("Do you think you can beat it?");
-				Thread.sleep(000);
+				Thread.sleep(3000);
 				introductionText.setText("We will see.");
-				Thread.sleep(000);
+				Thread.sleep(3000);
 				introductionText.setText("You have 3 lives to beat 3 levels.\nThere are 5 minigames per level.\nLose a minigame, lose a life.\nLose all your lives, game over.");
-				Thread.sleep(000);
+				Thread.sleep(6000);
 				Platform.runLater(() -> pane.getChildren().clear());
 				Platform.runLater(() -> runFirstMinigame(primaryStage));
 			} catch (InterruptedException e) {
@@ -91,7 +91,7 @@ public class Capstone extends Application{
 	//TODO Make sure after 5th minigame their is an endgame check
 	//Run the first minigame after the intro is done
 	private void runFirstMinigame(Stage primaryStage) {
-		pane = new DodgeMinigame(level); //TODO change back to first minigame
+		pane = new CirclesMinigame(level); //TODO change back to first minigame
 		scene = new Scene(pane, WINDOW_SIZE, WINDOW_SIZE);
 		primaryStage.setScene(scene);
 	}
@@ -174,7 +174,7 @@ public class Capstone extends Application{
 				} else {
 					//Put next minigame into stage
 					Platform.runLater(() -> {
-						pane = new CirclesMinigame(level); //TODO change
+						pane = new DodgeMinigame(level);
 						scene = new Scene(pane, WINDOW_SIZE, WINDOW_SIZE);
 						primaryStage.setScene(scene);
 					});
